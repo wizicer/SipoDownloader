d41f525ae4c71fb9f44e86cbf963b690e92f7850
Icer <liangshuang@uchaindb.com>
2019-10-27 15:21:47 +0800

    parse page

--- PatentFormVer/MainForm.cs
+++ PatentFormVer/MainForm.cs
@@ -9,6 +9,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -54,70 +55,130 @@ namespace PatentFormVer
                     //browser.SetCookies(new Uri(cookie.Domain), $"{cookie.Name}={cookie.Value}");
                 }
 
-                var d = browser.GetCookie(new Uri("http://epub.sipo.gov.cn"), "JSESSIONID");
+                //var d = browser.GetCookie(new Uri("http://epub.sipo.gov.cn"), "JSESSIONID");
 
-
-
-                //<div class="cp_img">
-                //	<img onerror="javascript:this.src='images/cp_noimg.jpg';" src="pic/wshgg7100/PUBXML/20060906/WGSQ/WGSQ_DZGBD/2005301387738/130001/000003_thumb.jpg">
-                //		</div>
-                //<div class="cp_linr">
-                //	<h1>
-                //		[外观设计]&nbsp;小饰物（Pop-It Key Chain-Traditional)</h1>
-                //	<ul>
-                //		<li class="wl228">授权公告号：CN3558385</li>
-                //				<li class="wl228">授权公告日：2006.09.06</li>
-                //			<li class="wl228">申请号：2005301387738</li>
-                //		<li class="wl228">申请日：2005.10.20</li>
-                //		<li class="wl228">专利权人：威恩·苛恩</li>
-                //		<li class="wl228">设计人：威恩·苛恩</li>
-                //		<li class="clear"></li>
-                //		<li>地址：香港上环干诺道西88号粤财大厦29楼</li>
-                //		<li>分类号：11-02&nbsp;&nbsp;<a href="javascript:;" class="zhankai" style="color:#c5000f">全部</a>
-                //			<div style="display:none;"><ul>
-                //				<li>专利代理机构：广州华进联合专利商标代理有限公司</li><li>代理人：曾旻辉</li></ul></div>
-                //		</li>
-                //		</ul>
-                //	<div class="cp_jsh">
-                //		<span id="tit">
-                //		简要说明：</span>
-                //		左视图与右视图对称，省略左视图。</div>
-                //	<p class="cp_botsm">
-                //		<a href="javascript:zl_xm('2005301387738','pdg');">【全部数据】</a>
-                //			<span><a href="javascript:sw_xx('2005301387738');">事务数据</a></span>
-                //	</p>
-                //</div>
-                //<a class="qrcode" id="CN3558385" href="qrcode/CN3558385.png"><img src="qrcode/CN3558385.png" width="74" height="74"></a>
-
-
-
-
-                // strSources
-                // pip: 发明公布
-                // pig: 发明授权
-                // pug: 实用新型
-                // pdg: 外观设计
-                var type = "pdg";
                 //var homePage = browser.NavigateToPage(new Uri("http://epub.sipo.gov.cn/gjcx.jsp"));
-                var homePage = browser.NavigateToPage(
-                    new Uri("http://epub.sipo.gov.cn/patentoutline.action"),
-                    HttpVerb.Post,
-                    $"showType=1&strSources={type}&strWhere=%28TI%3D%27区块链%27%29&numSortMethod=0&strLicenseCode=&numIp=&numIpc=&numIg=&numIgc=&numIgd=&numUg=&numUgc=&numUgd=&numDg=0&numDgc=&pageSize=10&pageNow=1");
 
                 //var form = homePage.FindFormById("pato");
                 //form["strWhere"] = "(TI='区块链')";
                 //form["pageSize"] = "10";
                 //form["pageNow"] = "1";
                 //var resultsPage = form.Submit();
-
                 //var cps = resultsPage.Html.CssSelect("div.cp_box").ToArray();
-                var cps = homePage.Html.CssSelect("div.cp_box").ToArray();
-                foreach (var cp in cps)
-                {
-                    var imgElm = cp.CssSelect("div.cp_img img").FirstOrDefault();
-                    var img = imgElm.GetAttributeValue("src");
-                }
+
+                ParsePage(browser, SourceType.InventGrant, "区块链", 1);
             }
         }
+
+        private static PageInfo ParsePage(ScrapingBrowser browser, SourceType type, string keywords, int pageNumber)
+        {
+            var typeStr
+                = type == SourceType.DesignGrant ? "pdg"
+                : type == SourceType.InventGrant ? "pig"
+                : type == SourceType.InventPublish ? "pip"
+                : type == SourceType.UtilityGrant ? "pug"
+                : throw new NotSupportedException("unknown type");
+            var homePage = browser.NavigateToPage(
+                new Uri("http://epub.sipo.gov.cn/patentoutline.action"),
+                HttpVerb.Post,
+                $"showType=1&strSources={typeStr}&strWhere=%28TI%3D%27{keywords}%27%29" +
+                $"&numSortMethod=0&strLicenseCode=&numIp=&numIpc=&numIg=&numIgc=&numIgd=&numUg=&numUgc=&numUgd=&numDg=0&numDgc=" +
+                $"&pageSize=10&pageNow={pageNumber}");
+            var cps = homePage.Html.CssSelect("div.cp_box").ToArray();
+            var list = cps.Select(_ => ParseItem(_)).ToArray();
+            //var curPage = homePage.Html.CssSelect("div.next a.hover").FirstOrDefault()?.InnerText;
+            var pages = homePage.Html.CssSelect("div.next a").ToArray();
+            var regPage = new Regex(@"\((?<g>[0-9]+)\)");
+            var maxNumber = 0;
+            var nextNumber = 0;
+            var curNumber = 0;
+            foreach (var page in pages)
+            {
+                var href = page.GetAttributeValue("href");
+                var d = regPage.Match(href).Groups["g"].Value;
+                if (int.TryParse(d, out var number))
+                {
+                    if (number > maxNumber) maxNumber = number;
+                    if (page.InnerText == "&gt;")
+                    {
+                        nextNumber = number;
+                    }
+                    else if (page.GetClasses().Contains("hover"))
+                    {
+                        curNumber = number;
+                    }
+                }
+            }
+
+            return new PageInfo
+            {
+                Items = list,
+                NextPage = nextNumber,
+                MaxPage = maxNumber,
+                CurrentPage = curNumber,
+                Raw = homePage.Html.OuterHtml.Trim(),
+            };
+        }
+
+        private static CrudeInfo ParseItem(HtmlAgilityPack.HtmlNode cp)
+        {
+            var imgElm = cp.CssSelect("div.cp_img img").FirstOrDefault();
+            var img = imgElm?.GetAttributeValue("src");
+            var headElm = cp.CssSelect("div.cp_linr h1").FirstOrDefault();
+            var head = headElm?.InnerText;
+            var detailElm = cp.CssSelect("div.cp_linr ul").FirstOrDefault();
+            var details = detailElm?.InnerHtml;
+            var descElm = cp.CssSelect("div.cp_linr div.cp_jsh").FirstOrDefault();
+            var desc = descElm?.InnerHtml;
+            var linksElm = cp.CssSelect("div.cp_linr p.cp_botsm").FirstOrDefault();
+            var links = linksElm?.InnerHtml;
+            var qrElm = cp.CssSelect("a.qrcode img").FirstOrDefault();
+            var qr = qrElm?.GetAttributeValue("src");
+            var idElm = cp.CssSelect("a.qrcode").FirstOrDefault();
+            var id = idElm?.GetAttributeValue("id");
+
+            return new CrudeInfo
+            {
+                Id = id.Trim(),
+                Image = img.Trim(),
+                Title = head.Trim(),
+                Details = details.Trim(),
+                Description = desc.Trim(),
+                Links = links.Trim(),
+                QrImage = qr.Trim(),
+                Raw = cp.OuterHtml.Trim(),
+            };
+        }
+    }
+
+    public enum SourceType
+    {
+        // pip: 发明公布
+        InventPublish,
+        // pig: 发明授权
+        InventGrant,
+        // pug: 实用新型
+        UtilityGrant,
+        // pdg: 外观设计
+        DesignGrant,
+    }
+    public class PageInfo
+    {
+        public CrudeInfo[] Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int NextPage { get; set; }
+        public int MaxPage { get; set; }
+        public string Raw { get; set; }
+    }
+    public class CrudeInfo
+    {
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public string Description { get; set; }
+        public string Links { get; set; }
+        public string QrImage { get; set; }
+        public string Raw { get; set; }
     }
 }
