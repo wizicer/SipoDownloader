0e44628ed08a619b221baa74174e75123a6d3ab5
Icer <liangshuang@uchaindb.com>
2019-10-29 06:27:31 +0800

    refactoring cookie refresh mechanism

--- PatentFormVer/MainForm.cs
+++ PatentFormVer/MainForm.cs
@@ -1,47 +1,37 @@
-﻿using CefSharp;
-using CefSharp.WinForms;
-using Newtonsoft.Json;
-using PatentFormVer.Entity;
-using ScrapySharp.Extensions;
-using ScrapySharp.Network;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PatentFormVer
+﻿namespace PatentFormVer
 {
+    using CefSharp;
+    using CefSharp.WinForms;
+    using Newtonsoft.Json;
+    using PatentFormVer.Entity;
+    using ScrapySharp.Extensions;
+    using ScrapySharp.Network;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     public partial class MainForm : Form
     {
-        private readonly ChromiumWebBrowser webBrowser;
         private readonly string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "works");
 
-        private ScrapingBrowser scrapeBrowser = new ScrapingBrowser();
-        private bool refreshed = false;
-        private bool searching = false;
         private readonly string pageUrl = "http://epub.sipo.gov.cn/gjcx.jsp";
 
         public MainForm()
         {
             InitializeComponent();
-            webBrowser = new ChromiumWebBrowser(pageUrl);
-            webBrowser.FrameLoadEnd += WebBrowser_FrameLoadEnd;
-            webBrowser.LoadingStateChanged += WebBrowser_LoadingStateChanged;
-            webBrowser.Dock = DockStyle.Fill;
-            this.splitContainer1.Panel1.Controls.Add(webBrowser);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            this.DisableSearch();
         }
 
         private void DisableSearch()
@@ -58,48 +48,14 @@ namespace PatentFormVer
             }));
         }
 
-        private void WebBrowser_LoadingStateChanged(object sender, CefSharp.LoadingStateChangedEventArgs e)
-        {
-            if (!e.IsLoading)
-            {
-            }
-        }
-
-        private async void WebBrowser_FrameLoadEnd(object sender, CefSharp.FrameLoadEndEventArgs e)
-        {
-            await SetCookie();
-        }
-
-        private async Task SetCookie()
-        {
-            var result = await this.webBrowser.GetCookieManager().VisitUrlCookiesAsync("http://epub.sipo.gov.cn/gjcx.jsp", true);
-            if (result.Any(_ => _.Name == "JSESSIONID"))
-            {
-                scrapeBrowser.Encoding = Encoding.UTF8;
-
-                foreach (var cookie in result)
-                {
-                    scrapeBrowser.SetCookies(new Uri("http://epub.sipo.gov.cn"), $@"{cookie.Name}={cookie.Value}; expires={cookie.Expires}; path=/");
-                }
-
-                this.refreshed = true;
-                if (!this.searching)
-                {
-                    EnableSearch();
-                }
-            }
-        }
-
         private async void btnSearch_Click(object sender, EventArgs e)
         {
-            this.searching = true;
             DisableSearch();
-            await Search(scrapeBrowser, txtKeywords.Text, SourceType.InventPublish);
-            this.searching = false;
+            await Search(txtKeywords.Text, SourceType.InventPublish);
             EnableSearch();
         }
 
-        private async Task Search(ScrapingBrowser browser, string keywords, SourceType type)
+        private async Task Search(string keywords, SourceType type)
         {
             var pageNum = 1;
             var items = new List<RawGrantItemInfo>();
@@ -107,13 +63,11 @@ namespace PatentFormVer
             while (true)
             {
                 AddStatus($"Working on page {pageNum}/{maxPage}");
-                var page = await ParsePageAsync(browser, type, keywords, pageNum);
+                var page = await ParsePageAsync(type, keywords, pageNum);
                 items.AddRange(page.Items);
                 SaveEachItem(page.Items);
                 SaveSearch(keywords, type, items);
                 AddStatus($"Saved items from page {pageNum}");
-
-                await SetNewCookie();
 
                 if (page.MaxPage == page.CurrentPage) break;
                 pageNum = page.NextPage;
@@ -124,12 +78,30 @@ namespace PatentFormVer
             }
         }
 
-        private async Task SetNewCookie()
+        private async Task SetNewCookie(ScrapingBrowser scrapeBrowser)
         {
-            this.refreshed = false;
-            this.webBrowser.Load(pageUrl);
+            var refreshed = false;
+            var webBrowser = new ChromiumWebBrowser(pageUrl);
+            webBrowser.FrameLoadEnd += async (s, e) =>
+            {
+                var result = await webBrowser.GetCookieManager().VisitUrlCookiesAsync("http://epub.sipo.gov.cn/gjcx.jsp", true);
+                if (result.Any(_ => _.Name == "JSESSIONID"))
+                {
+                    scrapeBrowser.Encoding = Encoding.UTF8;
 
-            while (!this.refreshed)
+                    foreach (var cookie in result)
+                    {
+                        scrapeBrowser.SetCookies(new Uri("http://epub.sipo.gov.cn"), $@"{cookie.Name}={cookie.Value}; expires={cookie.Expires}; path=/");
+                    }
+
+                    refreshed = true;
+                }
+            };
+
+            webBrowser.Dock = DockStyle.Fill;
+            this.splitContainer1.Panel1.Controls.Add(webBrowser);
+
+            while (!refreshed)
             {
                 await Task.Delay(100);
             }
@@ -139,19 +111,10 @@ namespace PatentFormVer
         {
             foreach (var item in items)
             {
-                {
-                    var filepath = Path.Combine(basePath, $@"{item.Id}/raw.json");
-                    EnsureDirectoryExist(filepath);
-                    var json = JsonConvert.SerializeObject(item);
-                    File.WriteAllText(filepath, json);
-                }
-
-                {
-                    var filepath = Path.Combine(basePath, $@"{item.Id}/info.json");
-                    EnsureDirectoryExist(filepath);
-                    var json = JsonConvert.SerializeObject(item.ToGrantInfo());
-                    File.WriteAllText(filepath, json);
-                }
+                var filepath = Path.Combine(basePath, item.Id);
+                EnsureDirectoryExist(filepath);
+                File.WriteAllText(Path.Combine(filepath, "raw.json"), JsonConvert.SerializeObject(item));
+                File.WriteAllText(Path.Combine(filepath, "info.json"), JsonConvert.SerializeObject(item.ToGrantInfo()));
             }
         }
 
@@ -165,7 +128,9 @@ namespace PatentFormVer
 
         private void EnsureDirectoryExist(string path)
         {
-            var dir = Path.GetDirectoryName(path);
+            var dir = string.IsNullOrWhiteSpace(Path.GetExtension(path))
+                ? path
+                : Path.GetDirectoryName(path);
             if (Directory.Exists(dir)) return;
             Directory.CreateDirectory(dir);
         }
@@ -180,8 +145,10 @@ namespace PatentFormVer
             }));
         }
 
-        private static async Task<GrantListPageInfo> ParsePageAsync(ScrapingBrowser browser, SourceType type, string keywords, int pageNumber)
+        private async Task<GrantListPageInfo> ParsePageAsync(SourceType type, string keywords, int pageNumber)
         {
+            var browser = new ScrapingBrowser();
+            await this.SetNewCookie(browser);
             var typeStr = type.ToTypeString();
             var homePage = await browser.NavigateToPageAsync(
                 new Uri("http://epub.sipo.gov.cn/patentoutline.action"),
@@ -228,8 +195,7 @@ namespace PatentFormVer
                         case GrantItemPamLink gipl:
                             AddStatus($"work on {i + 1}/{dirs.Length} {link.Title} {gipl.Id}");
 
-                            await SetNewCookie();
-                            var pam = await ParsePamAsync(this.scrapeBrowser, gipl.Type, gipl.Id, gipl.Index);
+                            var pam = await ParsePamAsync(gipl.Type, gipl.Id, gipl.Index);
                             File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(pam));
 
                             using (var wc = new TimeoutWebClient(10000))
@@ -254,8 +220,10 @@ namespace PatentFormVer
             }
         }
 
-        private static async Task<PamPageInfo> ParsePamAsync(ScrapingBrowser browser, string type, string id, string number)
+        private async Task<PamPageInfo> ParsePamAsync(string type, string id, string number)
         {
+            var browser = new ScrapingBrowser();
+            await this.SetNewCookie(browser);
             var homePage = await browser.NavigateToPageAsync(
                 new Uri("http://epub.sipo.gov.cn/pam.action"),
                 HttpVerb.Post,
